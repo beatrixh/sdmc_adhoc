1b2cf88f9759cb512b5a72dec29c28804bf47a52
Beatrix Haddock <bhaddock@statsrv.pc.scharp.org>
2024-05-24 09:07:30 -0700

    simplifying script

--- processing_scripts/HVTN128/total_protein/process_data.py
+++ processing_scripts/HVTN128/total_protein/process_data.py
@@ -11,12 +11,12 @@ import sdmc_tools.process as sdmc
 import sdmc_tools.constants as constants
 ## ---------------------------------------------------------------------------##
 def main():
-    yaml_path = os.path.dirname(__file__) + "/paths.yaml"
+    yaml_path = "/home/bhaddock/repos/sdmc-adhoc/processing_scripts/HVTN128/total_protein" + "/paths.yaml"
     with open(yaml_path, 'r') as file:
         yaml_dict = yaml.safe_load(file)
 
     input_data_path = yaml_dict['input_data_path']
-    total_protein.columns = [i.lower().replace(" ","_") for i in total_protein.columns]
+    total_protein = pd.read_csv(input_data_path)
 
     total_protein = total_protein.rename(columns={
         "global_identifier":"guspec",
@@ -25,110 +25,36 @@ def main():
 
     ldms = pd.read_csv(constants.LDMS_PATH_HVTN, usecols=constants.STANDARD_COLS)
     ldms = ldms.loc[ldms.lstudy==128.].drop_duplicates()
+    ldms = ldms.loc[ldms.guspec.isin(total_protein.guspec.unique().tolist())]
 
-    dh = sdmc.DataHandler(
-        input_data = total_protein,
-        guspec_col = 'guspec',
-        network = 'hvtn'
-    )
-    dh.ldms = ldms.copy()
-
-    # merge ldms
-    dh.add_ldms(cols = constants.STANDARD_COLS,
-                incl_spec_type=True,
-                map_drawdt=True,
-                relabel=True,
-                enforce_typing=True)
-
-    constants.SPEC_TYPE_DEFN_MAP = {
-        ('BLD','PLA'): 'Plasma',
-        ('BLD','SER'): 'Serum',
-        ('REC','SPG'): 'Rectal Sponge',
-        ('REC','SUP'): 'Rectal Biopsy',
-        ('CER','SPG'): 'Cervical Sponge',
-        ('CER','SUP'): 'Cervical Biopsy',
-        ('VAG','SUP'): 'Vaginal Biopsy',
-        ('VAG','WCK'): 'Vaginal Weck',
-        ('SAL','FLD'): 'Saliva',
-        ('SAL','SAL'): 'Saliva',
-        ('SEM','SEM'): 'Semen',
-        ('SEM','FLD'): 'Semen',
-        ('BLD','CEL'): 'PBMC',
-        ('BLD', 'CSR'): 'Serum',
-        ('BLD', 'DBS'): 'Dried Blood Spot',
-        ('BLD', 'LYS'): 'Whole Blood (Lysed)',
-        ('VCS', 'FLD'): 'Cervicovaginal Secretions (Fluid)',
-        ('VCS', 'MUC'): 'Cervicovaginal Secretions (Mucus)',
-        ('VAG', 'SWB'): 'Vaginal Swab',
-        ('BLD', 'BLD'): 'Whole Blood',
-        ('VCS', "N/A"): 'Cervicovaginal Secretions',
-        ('BLD', "N/A"): 'Whole Blood',
-        ('VCS', 'SWB'): 'Cervicovaginal Secretions (Swab)',
-        ('REC', 'BPS'): 'Rectal Biopsy',
-        ('VAG', 'BPS'): 'Vaginal Biopsy',
-        ('CER', 'BPS'): 'Cervical Biopsy',
-        ('REC', 'SEC'): 'Rectal Secretions'
-    }
-    dh.processed_data.spectype = dh.processed_data.apply(
-        lambda x: constants.SPEC_TYPE_DEFN_MAP[x.spec_primary, x.spec_derivative],
-        axis=1
-    )
-
-    if len(dh.processed_data.spectype.isna().sum()) > 0:
-        raise Exception("THERE ARE NANS IN SPEDCTYPE; NEED TO FIX")
-
-    dh.add_metadata({
+    metadata_dict = {
         "network": "HVTN",
         "upload_lab_id": "GT",
         "assay_lab_name": "Tomaras Lab (Duke)",
         "assay_kit": "Quant-iT",
         "assay_type": "Total Protein",
         "specrole": "Sample",
-    })
+    }
 
-    dh.add_sdmc_processing_info(input_data_path=input_data_path)
+    outputs = sdmc.standard_processing(
+        input_data=total_protein,
+        input_data_path=input_data_path,
+        guspec_col="guspec",
+        network='hvtn',
+        metadata_dict=metadata_dict,
+        ldms=ldms
+    )
 
-    outputs = dh.processed_data.copy()
+    if len(outputs.spectype.isna().sum()) > 0:
+        raise Exception("THERE ARE NANS IN SPEDCTYPE; NEED TO FIX")
 
     outputs = outputs.drop(columns=[
         "assay_platform", "lab", "study", "visit_identifier",
         "timepoint", "timepoint_units", "isotype", "sample_identifier"
     ])
 
-    reorder = [
-        'network',
-        'protocol',
-        'specrole',
-        'guspec',
-        'ptid',
-        'visitno',
-        'drawdt',
-        'spectype',
-        'spec_primary',
-        'spec_additive',
-        'spec_derivative',
-        'upload_lab_id',
-        'assay_lab_name',
-        'assay_type',
-        'assay_kit',
-        'analyte',
-        'concentration_units',
-        'description',
-        'experiment_date',
-        'experiment_identifier',
-        'concentration',
-        'sample_role',
-        'sample_type',
-        'sdmc_processing_datetime',
-        'sdmc_data_receipt_datetime',
-        'input_file_name'
-    ]
-    if set(reorder).symmetric_difference(outputs.columns):
-        raise Exception("TRYING TO DROP OR ADD COLUMNS ON REORDER; CHECK")
-
-    outputs = outputs[reorder]
-
     today = datetime.date.today().isoformat()
+    savedir = yaml_dict['savedir']
     savepath = savedir + f'DRAFT_HVTN128_total_protein_processed_{today}.txt'
     outputs.to_csv(savepath, index=False, sep="\t")
 ## ---------------------------------------------------------------------------##
@@ -142,7 +68,7 @@ def pivot():
                                   )
 
     pivot_summary = pivot_summary.droplevel(level=0, axis=1)
-    summary_savepath = f'/networks/vtn/lab/SDMC_labscience/studies/HVTN/HVTN128/assays/total_protein/misc_files/data_processing/HVTN128_total_protein_pivot_summary.xlsx'
+    summary_savepath = savedir + f'HVTN128_total_protein_pivot_summary.xlsx'
     pivot_summary.to_excel(summary_savepath)
 
     ## Checks --------------------------------------------------------------------##
@@ -159,6 +85,11 @@ def pivot():
 
     print("guspec of the rectal biopsy guspec thats in the manifest but NOT in the smc data:")
     print(manifest.loc[~(manifest.guspec.isin(outputs.guspec)) & (manifest.material_type=="Rectal Biopsy")].guspec)
+
+    if len(set(manifest.subject_id.astype(int)).symmetric_difference(outputs.ptid.astype(int))) == 0:
+        print("we have a match between prtids in manifest and results")
+    else:
+        print("mismatch between results and manifest")
 
 if __name__ == '__main__':
     main()
