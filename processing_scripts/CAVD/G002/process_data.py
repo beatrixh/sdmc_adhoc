761fbf6605f713a44e90aa1b235032c3b65015e1
Beatrix Haddock <bhaddock@statsrv.pc.scharp.org>
2024-07-23 15:49:37 -0700

    updated to new filepath, fixed floating point error, added cavd metadata

--- processing_scripts/CAVD/G002/process_data.py
+++ processing_scripts/CAVD/G002/process_data.py
@@ -7,9 +7,6 @@ import pandas as pd
 import numpy as np
 import os
 import datetime
-import yaml
-import sdmc_tools.process as sdmc
-import sdmc_tools.constants as constants
 ## ---------------------------------------------------------------------------##
 
 def main():
@@ -21,7 +18,6 @@ def main():
             nrows -= 1
             data = pd.read_csv(path, encoding='iso-8859-1', sep="\t", skiprows=nrows)
         data['ptid'] = path.split("/")[-1].split("_")[0]
-        # data.ptid = data.ptid.astype(int)
         data['filter_pass'] = path.split("/")[-1].split(".")[0].split("_")[-1]
         usecols = ['ptid', 'Name', 'Block', 'filter_pass', 'F488 Mean', 'B488 Mean']
         data = data.loc[~data.Name.isin(["empty","Empty","DyeSpot"]),usecols]
@@ -34,7 +30,7 @@ def main():
         return data
 
     parentdir = '/networks/cavd/VDCs/Schief/Schief_856-G002/SkinReactions/data/Glycan_array_Scripps/20240327-01/OneDrive_1_3-27-2024/'
-    data = pd.DataFrame()
+    input_data = pd.DataFrame()
     for site in os.listdir(parentdir):
         datadir = parentdir + "/"  + site + "/Processed Data/"
         files = os.listdir(datadir)
@@ -43,22 +39,29 @@ def main():
         for file in files:
             sub = pd.concat([sub, get_data(datadir + file)])
         sub["site"] = site
-        data = pd.concat([data, sub])
+        input_data = pd.concat([input_data, sub])
 
     # verify that there are 6 rows per unique combo
-    t = data.groupby(['ptid','site','spot_name','filter_pass','block']).count().reset_index()
-    if not t.f488_mean.unique().tolist==[6]:
-        raise Exception("There should be 6 rows per unique sample/spot name/filter/block!")
-
-    # add primary result column
-    data['f488_mean_minus_b488_mean'] = data.f488_mean - data.b488_mean
+    t = input_data.groupby(['ptid','site','spot_name','filter_pass','block']).count().reset_index()
+    count_per = t.f488_mean.unique().tolist()
+    if len(count_per) > 1 or count_per[0]!=6:
+        print(f"t.f488_mean.unique(): {t.f488_mean.unique()}")
+        raise Exception("There should be 6 rows per unique spit/site/spot name/filter/block!")
 
     # subset to 'hi' pass only, per the lab
-    data = data.loc[data.filter_pass=="Hi"]
+    input_data = input_data.loc[input_data.filter_pass=="Hi"]
+
+    # add primary result column
+    data = input_data.copy()
+    data['f488_mean_minus_b488_mean'] = np.round(input_data.f488_mean - input_data.b488_mean, 2)
 
     # Merge on timepoint column ----------------------------------------------##
     sample_id_path = '/networks/vtn/lab/SDMC_labscience/operations/documents/templates/assay/template_testing/IAVI-G002_ArrayDataKey.xlsx'
     sample_id_df = pd.read_excel(sample_id_path)
+
+    # make sure ptid/visit uniquely identifies
+    if len(sample_id_df[['PTID','Visit']].drop_duplicates())!=len(sample_id_df):
+        raise Exception("ptid/visit arent a unique identifier in lab sample mapping!")
 
     # verify that these are an exact match
     if len(set(sample_id_df.PTID).symmetric_difference(data.ptid)) > 0:
@@ -92,6 +95,34 @@ def main():
 
     # drop duplicate columns
     data = data.drop(columns = ['Origin', 'PTID', 'Study Week'])
+
+    # Merge on CAVD metadata (incl sample identifier) ----------------------------------------------##
+    cavd_sample_list_path = "/networks/cavd/VDCs/Schief/Schief_856-G002/SkinReactions/samples/Skin AE sample testing.xlsx"
+    cavd_sample_list = pd.read_excel(cavd_sample_list_path)
+    cavd_sample_list = cavd_sample_list.loc[cavd_sample_list.Destination=="GCF"]
+
+    data.Visit = data.Visit.astype(int)
+
+    cavd_sample_list = cavd_sample_list.rename(columns = {"Subject ID": "ptid", "Study Day":"study_week"})
+    cavd_sample_list['Date Drawn'] = cavd_sample_list['Date Drawn'].dt.date
+
+    data = data.merge(cavd_sample_list, on=["Study ID", "ptid", "Visit", "study_week"], how="left")
+
+    # verify that there are still 6 rows per unique combo
+    t = data.groupby(['Current Label', 'spot_name', 'block']).count().reset_index()
+    count_per = t.f488_mean.unique().tolist()
+    if len(count_per) > 1 or count_per[0]!=6:
+        print(f"t.f488_mean.unique(): {t.f488_mean.unique()}")
+        raise Exception("There should be 6 rows per unique sample/spot_name/block!")
+
+    compare = cavd_sample_list[['ptid','study_week']].merge(
+        sample_id_df,
+        left_on=['ptid', 'study_week'],
+        right_on=['PTID',"Study Week"], how = 'outer'
+    )
+
+    if len(compare)!=len(cavd_sample_list):
+        raise Exception("lab sample list and cavd sample list don't match!")
 
     ## merge on glycan info --------------------------------------------------##
     glycan_info_path = "/networks/cavd/VDCs/Schief/Schief_856-G002/SkinReactions/data/Glycan_array_Scripps/Scheif-GlycanArrayList-Final.xlsx"
@@ -130,10 +161,10 @@ def main():
         "lab_software_version": ["Mapix"],
         "specrole": ["Sample"],
         "glycan_mapping_file": ["Scheif-GlycanArrayList-Final.xlsx"],
-        "sample_mapping_file": ["IAVI-G002_ArrayDataKey.xlsx"],
+        "lab_sample_mapping_file": ["IAVI-G002_ArrayDataKey.xlsx"],
+        "cavd_sample_mapping_file": ["Skin AE sample testing.xlsx"],
         "fred_hutch_processing_timestamp": [fred_hutch_processing_timestamp],
         "network": "CAVD",
-        "sample_id": "NA",
     }
 
     data = data.merge(pd.DataFrame(metadata_dict), how='cross')
@@ -144,7 +175,10 @@ def main():
     data = data.rename(columns = {
         'filename':'input_file_name',
         'blocks': 'block_group',
-        'study_id': 'lab_submitted_study_id'
+        'study_id': 'lab_submitted_study_id',
+        'current_label': 'sample_id',
+        'volume': 'sample_volume_total',
+        'volume_unit': 'sample_volume_unit'
     })
 
     reorder = [
@@ -154,8 +188,11 @@ def main():
         'ptid',
         'visit',
         'study_week',
-        'block_group',
+        'date_drawn',
         'block',
+        'block_group',
+        'material_type',
+        'material_modifiers',
         'specrole',
         'upload_lab_id',
         'assay_lab_name',
@@ -165,6 +202,9 @@ def main():
         'assay_precision',
         'assay_details',
         'site',
+        'group',
+        'destination',
+        'requisition_id',
         'spot_name',
         'glycan_m_number',
         'glycan_structure',
@@ -172,9 +212,12 @@ def main():
         'b488_mean',
         'f488_mean_minus_b488_mean',
         'filter_pass',
+        'sample_volume_total',
+        'sample_volume_unit',
         'input_file_name',
         'glycan_mapping_file',
-        'sample_mapping_file',
+        'lab_sample_mapping_file',
+        'cavd_sample_mapping_file',
         'input_data_timestamp',
         'fred_hutch_processing_timestamp'
     ]
@@ -185,10 +228,12 @@ def main():
 
     outputs = data[reorder]
 
+    outputs.sample_id = outputs.sample_id.astype(str)
+
     ## save results ----------------------------------------------------------##
-    savedir = "/networks/vtn/lab/SDMC_labscience/studies/VISC/G002/data_processing/"
-    timestamp = datetime.date.today().isoformat()
-    outputs.to_csv(savedir + "EXAMPLE_CAVD_G002_Glycan_Microarray_data_processed_{today}.txt", sep="\t", index=False)
+    savedir = "/networks/cavd/VDCs/Schief/Schief_856-G002/SkinReactions/data/Glycan_array_Scripps/processed_data/"
+    today = datetime.date.today().isoformat()
+    outputs.to_csv(savedir + f"DRAFT_CAVD_G002_Glycan_Microarray_data_processed_{today}.txt", sep="\t", index=False)
 
 if __name__=="__main__":
     main()
