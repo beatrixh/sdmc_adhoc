d7b4f684d292022b6c1d1392ea215d6c055645db
Beatrix Haddock <bhaddock@statsrv.pc.scharp.org>
2025-03-31 13:58:32 -0700

    updates for LDMS transition to delphi

---
+++ access_ldms.py
@@ -0,0 +1,252 @@
+import pandas as pd
+import numpy as np
+import psycopg
+import yaml
+
+STANDARD_COLS = ['txtpid',
+ 'drawdm',
+ 'drawdd',
+ 'drawdy',
+ 'vidval',
+ 'lstudy',
+ 'guspec',
+ 'primstr',
+ 'addstr',
+ 'dervstr']
+
+# grab my password
+yaml_path = "/home/bhaddock/repos/config.yaml"
+with open(yaml_path, 'r') as file:
+    config = yaml.safe_load(file)
+
+def get_cursor():
+    """
+    Get cursor into datamart
+    """
+    conn = psycopg.connect(host="datamart.scharp.org",
+                            dbname="datamart02_realtime",
+                            user="bhaddock",
+                            password=config['password'])
+
+    return conn.cursor()
+
+def pull_protocol_map() -> dict:
+    """
+    Return a dictionary that maps each protocol to the schema it is stored in.
+    e.g.,
+    {'hvtn82': 'hvtn082',
+    'hvtn139': 'hvtn139_dummy_arm',
+    'hvtn503': 'hvtn503',
+    'covpn3002': 'covpn3002'}
+    -----
+    DICTIONARY ARGS
+    - keys are of the form {network}{protocol}
+        - with no spaces,
+        - where 'network' is all lowercase,
+        - protocol is int-formatted with no leading zeros.
+        - EXCEPT: 'unmapped' and 'hvtn_unmapped' map to the unmapped hvtn protocol schema
+    """
+    # pull all schemas that have an imported_fstrf_specimen_aliquot table---------------------------------------##
+    cursor = get_cursor()
+    cursor.execute("""SELECT table_schema,table_name FROM information_schema.tables where table_name like 'imported_fstrf_specimen_aliquot'""")
+    tables = cursor.fetchall()
+
+    schemas = np.array(tables)[:,0]
+
+    schema_usemap = {}
+    for s in schemas:
+        protocol = s.split("_")[0]
+        if protocol not in schema_usemap:
+            schema_usemap[protocol] = [s]
+        else:
+            schema_usemap[protocol] += [s]
+
+    # grab all protocols with only one schema per protocol -----------------------------------------------------##
+    one_option_only = pd.DataFrame({k:v for (k,v) in schema_usemap.items() if len(v) == 1}).T.reset_index()
+    one_option_only.columns = ['network_protocol','Schema']
+    one_option_only['Use'] = True
+
+    one_option_only.loc[one_option_only.Schema=='hvtn_unmapped','network_protocol'] = 'hvtn_unmapped'
+
+    # now deal with protocols that have multiple schemas
+    multiples = {k:v for (k,v) in schema_usemap.items() if len(v) > 1}
+
+    which_to_use = pd.DataFrame([i for j in multiples.values() for i in j], columns=['schema'])
+    which_to_use['use'] = False
+    which_to_use = pd.concat([which_to_use.schema.str.split("_", expand=True)[0], which_to_use], axis=1)
+    which_to_use.columns = ['network_protocol', 'Schema', 'Use']
+
+    which_to_use.loc[which_to_use.Schema=="hvtn144_dummy_arm", 'Use'] = True
+    which_to_use.loc[which_to_use.Schema=="hvtn503", 'Use'] = True
+    which_to_use.loc[which_to_use.Schema=="hvtn139_dummy_arm", 'Use'] = True
+    which_to_use.loc[which_to_use.Schema=="hvtn312_dummy_arm", 'Use'] = True
+    which_to_use.loc[which_to_use.Schema=="hvtn605_dummy_arm", 'Use'] = True
+
+    # # add in HVTN503.1
+    # which_to_use.loc[len(which_to_use)] = ['hvtn503.1', 'hvtn503_1', True]
+    # which_to_use.loc[len(which_to_use)] = ['hvtn503_1', 'hvtn503_1', True]
+
+    # ensure that for all protocols with multiple schemas, we chose exactly one
+    check_one_per_protocol = which_to_use.groupby('network_protocol').Use.sum()
+    issues = check_one_per_protocol.loc[check_one_per_protocol != 1].index.tolist()
+    if len(issues) > 0:
+        raise Warning(f"For the following protocols, there are multiple schemas! Need to choose which to use: {issues}")
+
+    which_to_use = pd.concat([which_to_use, one_option_only], axis=0)
+
+    ## remove leading zeros in network_protocol names ---------------------------------------------------------##
+
+    # drop the unmapped network so can cast others to int
+    which_to_use = which_to_use.loc[~which_to_use.network_protocol.isin(['hvtn_unmapped', 'hvtn503_1', 'hvtn503.1'])]
+
+    # pull out network
+    which_to_use['network'] = ['hvtn' if 'hvtn' in i else 'covpn' for i in which_to_use.network_protocol]
+
+    # pull out network
+    which_to_use['protocol'] = [int(i[4:]) if i[:4]=='hvtn' else int(i[5:]) for i in which_to_use.network_protocol]
+
+    # concat network/protocol back together
+    which_to_use['key'] = which_to_use.network + which_to_use.protocol.astype(str)
+
+    # convert to dictionary, add unmapped back in
+    # WILL NEED TO UPDATE IF THEY ADD A COVPN UNMAPPED
+    which_to_use = which_to_use.loc[which_to_use.Use].set_index('key')['Schema'].to_dict()
+    which_to_use['unmapped'] = 'hvtn_unmapped'
+    which_to_use['hvtn_unmapped'] = 'hvtn_unmapped'
+    which_to_use['hvtn503.1'] = 'hvtn503_01'
+    which_to_use['hvtn503_1'] = 'hvtn503_01'
+
+    return which_to_use
+
+def pull_one_protocol(network, protocol, usecols=STANDARD_COLS):
+    """
+    ----
+    INPUT
+    network: network name. one of 'hvtn' or 'covpn'
+
+    usecols
+    -----
+    TODO: look into proper way to call conn.close()
+    """
+    if protocol=='unmapped':
+        network_protocol = 'hvtn_unmapped'
+    elif protocol in ['503.1', '503_1', 503.1, '503_01', '503.01', 503.01]:
+        network_protocol = 'hvtn503_01'
+    else:
+        if isinstance(protocol, str):
+            protocol = protocol.lower().replace(" ","")
+        network = network.lower().replace(" ","").replace("_","")
+        pmap = pull_protocol_map()
+        schemas = pd.DataFrame({'network_protocol':pmap.keys(), 'schema_name':pmap.values()})
+        schemas['network'] = ['hvtn' if 'hvtn' in i else 'covpn' for i in schemas.network_protocol]
+        schemas['protocol'] = [i[4:] if i[:4]=='hvtn' else i[5:] for i in schemas.network_protocol]
+        schemas.loc[schemas.schema_name.str.contains("unmapped"), 'protocol'] = np.nan
+        schemas['protocol'] = schemas.protocol.astype(float)
+
+        schema = schemas.loc[(
+            (schemas.network==network) &
+            (schemas.protocol==float(protocol))
+        )]
+        if len(schema) > 1:
+            raise Warning("ERROR: Network/Protocol combo returned more than one table")
+
+        network_protocol = f"{network}{int(protocol)}"
+        network_protocol = pmap[network_protocol]
+
+    # stringify columns to pull
+    if isinstance(usecols, list):
+        columns_to_pull = ', '.join(usecols)
+    # pull all if user passes 'all'
+    elif usecols == 'all':
+        columns_to_pull = "*"
+    else:
+        columns_to_pull = usecols
+
+    # get db cursor
+    cursor = get_cursor()
+
+    # pull data
+    cursor.execute(f"""SELECT {columns_to_pull} FROM {network_protocol}.imported_fstrf_specimen_aliquot""")
+    data = cursor.fetchall()
+
+    # if pulled all columns, get them as a list
+    if usecols == 'all':
+        cursor.execute(f"""select column_name from information_schema.columns where table_schema='{network_protocol}' AND table_name like 'imported_fstrf_specimen_aliquot' order by ordinal_position;""")
+        usecols = cursor.fetchall()
+        usecols = [i[0] for i in usecols]
+    if not isinstance(usecols, list):
+        usecols = [usecols]
+
+    # close connection
+    cursor.close()
+
+    return pd.DataFrame(data, columns=usecols)
+
+def pull_multiple_protocols(network, protocols, usecols=STANDARD_COLS):
+
+    # stringify columns to pull
+    if isinstance(usecols, list):
+        columns_to_pull = ', '.join(usecols)
+    # pull all if user passes 'all'
+    elif usecols == 'all':
+        columns_to_pull = "*"
+    else:
+        columns_to_pull = usecols
+
+    # format network_protocols correctly
+    network = network.lower().replace(" ","").replace("_","")
+    pmap = pull_protocol_map()
+    network_protocols = []
+    if protocols=='all':
+        network_protocols = list(set(pmap.values()))
+        network_protocols = [i for i in network_protocols if network in i]
+    else:
+        for protocol in protocols:
+            if protocol=='unmapped':
+                network_protocols += ['hvtn_unmapped']
+            else:
+                if isinstance(protocol, str):
+                    protocol = protocol.lower().replace(" ","")
+                schemas = pd.DataFrame({'network_protocol':pmap.keys(), 'schema_name':pmap.values()})
+                schemas['network'] = ['hvtn' if 'hvtn' in i else 'covpn' for i in schemas.network_protocol]
+                schemas['protocol'] = [i[4:] if i[:4]=='hvtn' else i[5:] for i in schemas.network_protocol]
+                schemas.loc[schemas.schema_name.str.contains("unmapped"), 'protocol'] = np.nan
+                schemas['protocol'] = schemas.protocol.astype(float)
+
+                schema = schemas.loc[(
+                    (schemas.network==network) &
+                    (schemas.protocol==float(protocol))
+                )]
+                if len(schema) > 1:
+                    raise Warning(f"ERROR: {network}/{protocol} combo returned more than one table")
+
+                network_protocol = f"{network}{int(protocol)}"
+                network_protocol = pmap[network_protocol]
+                network_protocols += [network_protocol]
+
+    # connect to db
+    cursor = get_cursor()
+
+    # build query
+    query_string = ''
+    for p in network_protocols:
+        query_string += f' UNION ALL SELECT {columns_to_pull} FROM {p}.imported_fstrf_specimen_aliquot'
+
+    query_string = query_string[11:]
+
+    # pull data
+    cursor.execute(query_string)
+    data = cursor.fetchall()
+
+    # if pulled all columns, get them as a list
+    if usecols == 'all':
+        cursor.execute(f"""select column_name from information_schema.columns where table_schema='{network_protocols[0]}' AND table_name like 'imported_fstrf_specimen_aliquot' order by ordinal_position;""")
+        usecols = cursor.fetchall()
+        usecols = [i[0] for i in usecols]
+    if not isinstance(usecols, list):
+        usecols = [usecols]
+
+    # close connection
+    cursor.close()
+
+    return pd.DataFrame(data, columns=usecols)
--- ldms_monitoring.py
+++ ldms_monitoring.py
@@ -1,34 +1,264 @@
-from ldms_monitoring_utilities import *
+## ---------------------------------------------------------------------------##
+# Author: Beatrix Haddock
+# Date: 03/12/2025
+# Purpose:
+#     - Save back-ups of subsets of LDMS
+#     - Check for changes since yesterday
+## ---------------------------------------------------------------------------##
+import pandas as pd, numpy as np
+import os
+import yaml
+import datetime
+from typing import List
+import sdmc_tools.constants as constants
+from yaml_handling import *
+from access_ldms import *
 
+def main():
+    protocols = {}
+    protocols['hvtn'] = yamldict["hvtn_protocols"]
+    protocols['covpn'] = yamldict["covpn_protocols"]
+
+    for network in ['hvtn','covpn']:
+        for protocol in protocols[network]:
+            save_todays_ldms(protocol, network)
+            delete_old_ldms(protocol, network)
+            detect_ldms_diffs(protocol, network)
+
+## constants and functions -------------------------------------------------- ##
 yamlpath = os.path.dirname(__file__) + "/constants.yaml"
 with open(yamlpath, 'r') as file:
     yamldict = yaml.safe_load(file)
 
-hvtn_protocols = yamldict["hvtn_protocols"]
-covpn_protocols = yamldict["covpn_protocols"]
+protocols = {}
+protocols['hvtn'] = yamldict["hvtn_protocols"]
+protocols['covpn'] = yamldict["covpn_protocols"]
 
-def check_for_updates(protocols: list, network: str) -> None:
-    # get today's timestamp
-    updated = was_ldms_updated(network)
-    if updated:
-        # save new copies of ldms
-        if len(protocols)>0:
-            # if there are at least two ldmss saved, delete the older ones
-            save_todays_ldms(protocols, network)
-            for protocol in protocols:
-                delete_old_ldms(protocol, network)
+PROTOCOL_DIRNAME_MAP = yamldict["PROTOCOL_DIRNAME_MAP"]
+GUSPEC_TO_OUTPUT_PATH_OLD = yamldict["GUSPEC_TO_OUTPUT_PATH_OLD"]
 
-        # determine if there are any updates, output corresponding alerts
-        for protocol in protocols:
-            try:
-                detect_ldms_diffs(int(protocol), network)
-            except:
-                print(f"ISSUE WITH {network}{int(protocol)}; CHECK CODE")
+def detect_ldms_diffs(PROTOCOL, NETWORK):
+    if NETWORK.lower() == 'covpn':
+        NETWORK = 'CoVPN'
+    elif NETWORK.lower() == 'hvtn':
+        NETWORK = 'HVTN'
     else:
-        print(f"{network} LDMS HASN'T BEEN UPDATED TODAY\n")
+        raise Exception(f"NETWORK must be one of 'HVTN' or 'CoVPN' (case insensitive). Submitted {NETWORK}")
 
-# print(f"hvtn protocols: {hvtn_protocols}\n")
-# print(f"covpn protocols: {covpn_protocols}\n")
+    ## pull old and new LDMS ------------------------------------------------ ##
+    today = datetime.date.today()
+    timestamp = today.strftime("%Y%m%d")
 
-check_for_updates(hvtn_protocols, "HVTN")
-check_for_updates(covpn_protocols, "CoVPN")
+    feed_dir = f"/networks/vtn/lab/SDMC_labscience/studies/{NETWORK}/{PROTOCOL_DIRNAME_MAP[NETWORK.upper()][int(PROTOCOL)]}/specimens/ldms_feed/"
+    fname = f"{NETWORK.lower()}.ldms{PROTOCOL}.{timestamp}.csv"
+
+    ## save LDMS for today if it doesn't exist
+    if not os.path.exists(feed_dir + fname):
+        save_todays_ldms(PROTOCOL, NETWORK)
+
+    ## read in todays LDMS
+    new = pd.read_csv(feed_dir + fname, dtype=constants.LDMS_DTYPE_MAP)
+    ## read in previous LDMS
+    files = os.listdir(feed_dir)
+    if len(files) > 1:
+        files = os.listdir(feed_dir)
+        files = [i for i in files if f'{NETWORK.lower()}.ldms{PROTOCOL}' in i]
+        prev_fname = np.sort(files)[-2]
+        old = pd.read_csv(feed_dir + prev_fname, dtype=constants.LDMS_DTYPE_MAP)
+        # print(f"{NETWORK}{PROTOCOL}: Comparing {fname} and {prev_fname}")
+    else:
+        print(f"NO PRIOR LDMS SAVED FOR {NETWORK}{PROTOCOL}")
+        old = pd.DataFrame()
+
+    # if either of these are empty, we have nothing to compare ----------------#
+    if old.empty or new.empty:
+        return
+
+    # the columns have to match -- otherwise have big problems ----------------#
+    # print(f"{NETWORK}{PROTOCOL}: Checking for col diff")
+    COL_DIFF = set(old.columns).symmetric_difference(new.columns)
+    if len(COL_DIFF) > 0:
+        print(f"{NETWORK}{PROTOCOL}: COLUMNS DON'T MATCH. THE FOLLOWING NOT SHARED: {COL_DIFF}")
+        return
+
+    # ensure column types / sorting match -------------------------------------#
+    col_sort_order = ['guspec', 'txtpid', 'drawdm', 'drawdd', 'drawdy', 'vidval', 'lstudy',
+       'primstr', 'addstr', 'dervstr']
+
+    for df in [old, new]:
+        for col in ['drawdm','drawdd','drawdy']:
+            df[col] = df[col].astype(int)
+        for col in ['vidval','lstudy']:
+            df[col] = df[col].astype(float)
+        df['txtpid'] = df.txtpid.astype(str)
+        df = df.sort_values(by=col_sort_order, ignore_index=True).drop_duplicates()
+
+    # were any guspecs removed ------------------------------------------------#
+    guspecs_removed = set(old.guspec).difference(new.guspec)
+    if guspecs_removed:
+        print(f"{NETWORK}{PROTOCOL}: THE FOLLOWING GUSPECS HAVE BEEN REMOVED FROM LDMS: {guspecs_removed}")
+        handle_affected_jobs(guspecs_removed)
+
+    # report on any added
+    guspecs_added = set(new.guspec).difference(old.guspec)
+    if guspecs_added:
+        print(f"{NETWORK}{PROTOCOL}: THE FOLLOWING GUSPECS WERE ADDED TO LDMS: {guspecs_added}")
+
+    # of the guspecs in both, is there still the name number of rows per guspec
+    shared_guspecs = set(old.guspec).intersection(new.guspec)
+
+    new = new.loc[new.guspec.isin(shared_guspecs)].drop_duplicates()
+    old = old.loc[old.guspec.isin(shared_guspecs)].drop_duplicates()
+
+    new_guspec_count = new.guspec.value_counts().to_frame()
+    old_guspec_count = old.guspec.value_counts().to_frame()
+
+    comp = new_guspec_count.merge(
+        old_guspec_count,
+        how='outer',
+        left_index=True,
+        right_index=True
+    )
+
+    GUSPEC_DIFFS = list(comp.loc[comp.count_x!=comp.count_y].index)
+
+    # report on any guspecs corresponding to different row counts -------------#
+    if GUSPEC_DIFFS:
+        print(f"{NETWORK}{PROTOCOL}: DIFFERENT NUMBER OF ROWS PER SHARED GUSPEC")
+        print(f"DIFFERENCES WITH THE FOLLOWING GUSPECS: {GUSPEC_DIFFS}")
+        handle_affected_jobs(GUSPEC_DIFFS)
+
+        print("Comparing matching rows:")
+        GUSPEC_MATCHES = list(comp.loc[comp.count_x==comp.count_y].index)
+        new = new.loc[new.guspec.isin(GUSPEC_MATCHES)]
+        old = old.loc[old.guspec.isin(GUSPEC_MATCHES)]
+
+    # did anything change among the guspecs that are in both ------------------#
+    # sort for comparison
+    new = new.sort_values(by=col_sort_order, ignore_index=True)
+    old = old.sort_values(by=col_sort_order, ignore_index=True)
+
+    # compare
+    diff = new.replace("N/A", np.nan).compare(old.replace("N/A", np.nan))
+    if len(diff) > 0:
+        print(f"CHANGES DETECTED IN SHARED GUSPECS")
+        AFFECTED_GUSPECS = list(new.iloc[diff.index].guspec.unique())
+        print(f"AFFECTED GUSPECS: {AFFECTED_GUSPECS}")
+        handle_affected_jobs(AFFECTED_GUSPECS)
+
+def handle_affected_jobs(guspecs: list) -> None:
+    handle_affected_jobs_old(guspecs)
+    handle_affected_jobs_new(guspecs)
+
+def handle_affected_jobs_old(guspecs: list) -> None: #report_outputs_corresponding_to_guspecs
+    """
+    given a list of guspecs with changes in ldms,
+    report on any that are used by older (non-auto rerunnable) jobs
+    """
+    affected_outputs = set()
+    for guspec in guspecs:
+        if guspec in GUSPEC_TO_OUTPUT_PATH_OLD.keys():
+            affected_outputs.update(GUSPEC_TO_OUTPUT_PATH_OLD[guspec])
+    if len(affected_outputs) > 0:
+        print(f"AFFECTED OUTPUTS: {list(AFFECTED_OUTPUTS)}")
+    else:
+        print("\nNO PRE-2024 OUTPUTS AFFECTED")
+
+def handle_affected_jobs_new(guspecs: list) -> None:
+    """
+    given a list guspecs with changes in ldms,
+    return a list of yaml dicts corresponding to jobs that used that guspec
+    """
+    # pull all yaml_dicts associated with jobs
+    # print("Checking to see if new (2024+) outputs affected\n")
+    # print("Finding yamls")
+    yamls = [
+        f for f in find_endpoints('/home/bhaddock/repos/sdmc-adhoc/processing_scripts', l=[]) if f.split("/")[-1]=="paths.yaml"
+    ]
+    # print("yamls obtained")
+    yamls = [read_yaml(p, add_path=True) for p in yamls]
+    affected_job_yamls = []
+    # print("looping through guspecs, checking if in yamls")
+    # print(f"guspecs to loop through: {guspecs}\n")
+    # print(f"yamls to loop through: {yamls}")
+    for guspec in guspecs:
+        for y in yamls:
+            if "guspecs" not in y.keys():
+                print("adding guspecs to yaml\n")
+                save_guspecs_to_yaml(y["yaml_path"])
+                y = read_yaml(y["yaml_path"], add_path=True)
+            if guspec in y["guspecs"]:
+                print(f"found guspec in an output: {guspec}")
+                affected_job_yamls += [y]
+    if len(affected_job_yamls) > 0:
+        print("\nNEW OUTPUTS AFFECTED")
+        for y in affected_job_yamls:
+            output_path = get_output_path_from_yaml(y)
+            print(f"! The following output file affected: {output_path}")
+            # rerun_affected_job(y)
+    else:
+        print("\nNO NEW (2024+) OUTPUTS AFFECTED")
+
+## delete old ldms ---------------------------------------------------------- ##
+def delete_old_ldms(PROTOCOL: str, NETWORK: str) -> None:
+    if NETWORK.lower() == 'covpn':
+        NETWORK = 'CoVPN'
+    elif NETWORK.lower() == 'hvtn':
+        NETWORK = 'HVTN'
+    else:
+        raise Exception(f"NETWORK must be one of 'HVTN' or 'CoVPN' (case insensitive). Submitted {NETWORK}")
+    PROTOCOL = int(PROTOCOL)
+    N = len(NETWORK) + len(".ldms") + len(str(PROTOCOL))
+    feed_dir = f"/networks/vtn/lab/SDMC_labscience/studies/{NETWORK}/{PROTOCOL_DIRNAME_MAP[NETWORK.upper()][int(PROTOCOL)]}/specimens/ldms_feed/"
+    things_in_dir = os.listdir(feed_dir)
+    # sort from oldest to most recent, and only take the ones that look like hvtn.ldmsPROTOCOL
+    applicable_things_in_dir = np.sort([i for i in things_in_dir if i[:N] == f'{NETWORK.lower()}.ldms{PROTOCOL}'])[::-1]
+
+    # delete all but the thirty most recent
+    for file in applicable_things_in_dir[30:]:
+        # print(f"DELETING THE FOLLOWING: {file}\n")
+        os.remove(feed_dir + file)
+
+def save_todays_ldms(PROTOCOL: str, NETWORK: str) -> None:
+    """
+    INPUTS
+     - PROTOCOLS: list of PROTOCOLS (numeric) from one network
+     - NETWORK: string ("HVTN" or "CoVPN") corresponding to above PROTOCOLS
+    """
+    if NETWORK.lower() == 'covpn':
+        NETWORK = 'CoVPN'
+    elif NETWORK.lower() == 'hvtn':
+        NETWORK = 'HVTN'
+    else:
+        raise Exception(f"NETWORK must be one of 'HVTN' or 'CoVPN' (case insensitive). Submitted {NETWORK}")
+
+    # read in ldms
+    ldms = pull_one_protocol(NETWORK, PROTOCOL)
+
+    # save ldms
+    timestamp = datetime.date.today().strftime("%Y%m%d")
+    network_path = f"/networks/vtn/lab/SDMC_labscience/studies/{NETWORK}/"
+    protocol_path = network_path + PROTOCOL_DIRNAME_MAP[NETWORK.upper()][int(PROTOCOL)]
+    if not os.path.exists(protocol_path):
+        print(f"creating protocol dir for {int(PROTOCOL)}")
+        os.mkdir(protocol_path)
+    specimen_path = protocol_path + "/specimens"
+    if not os.path.exists(specimen_path):
+        print(f"creating specimens dir for {int(PROTOCOL)}")
+        os.mkdir(specimen_path)
+    ldms_feed_path = specimen_path + "/ldms_feed"
+    if not os.path.exists(ldms_feed_path):
+        print(f"creating ldms_feed dir for {int(PROTOCOL)}")
+        print(ldms_feed_path)
+        os.mkdir(ldms_feed_path)
+
+    savepath = ldms_feed_path + f"/{NETWORK.lower()}.ldms{int(PROTOCOL)}.{timestamp}.csv"
+    if not os.path.exists(savepath):
+        # print(f"CREATING THE FOLLOWING: {savepath}")
+        ldms.to_csv(savepath, index=False)
+    else:
+        print(f"{savepath} already exists")
+
+
+if __name__=="__main__":
+    main()
