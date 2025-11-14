ae6a8a92cebc93764547b054b0669f99129090b1
Beatrix Haddock <bhaddock@statsrv.pc.scharp.org>
2025-11-14 15:42:26 -0800

    scripts for processing data

---
+++ processing_misc/CAVD914/Oschenbauer_CC_nAb/average_ic50_80s_tobit.R
@@ -0,0 +1,184 @@
+## ------------------------------------------------------------------------------------------##
+# Author: Beatrix Haddock
+# Date: 2025-11-14
+# Purpose: Calculate one IC50/80 per mAb-isolate pair from replicate estimates
+## ------------------------------------------------------------------------------------------##
+
+library(survival)
+library(dplyr)
+library(tidyr)
+library(psych)
+
+# read in data
+df = read.csv(
+  "N:/cavd/Studies/cvd914/qdata/LabData/NAB/CAVD914_NAB07_U6_20250924.txt",
+  sep='\t'
+)
+
+# formatting
+colnames(df) <- tolower(colnames(df))
+df <- df %>%
+  rename(
+    tetO_HIV_Env_IMC_name = isolate,
+    mab_name = guspec,
+    val = value
+  )
+
+
+# parse out isolate name
+df$isolate <- sapply(strsplit(df$tetO_HIV_Env_IMC_name, "\\."), function(x) substr(x[2], 2, nchar(x[2])))
+
+# parse out pubid
+df$pubid <- sapply(strsplit(df$isolate, "_"), function(x) paste(x[1], x[2], sep = "-"))
+
+# id vars
+df$mab_isolate_titer <- paste(df$mab_name, df$isolate, df$value_label, sep = "|")
+df$mab_isolate_pubid_titer <- paste(df$mab_name, df$isolate, df$pubid, df$value_label, sep = "|")
+
+# subset data
+
+df <- df[df$value_label %in% c("TITER_50_CURVE", "TITER_80_CURVE"),
+         c("dilution_min", "mab_isolate_pubid_titer", "mab_isolate_titer",
+           "mab_name", "isolate", "pubid", "value_label", "val")]
+
+# categorical vals
+df$isolate <- factor(df$isolate)
+df$mab_name <- factor(df$mab_name)
+df$pubid <- factor(df$pubid)
+
+# create flag for rows above/below cutoff
+df$lod = ""
+df$lod[which(grepl("^>", df$val))] = ">"
+df$lod[which(grepl("^<", df$val))] = "<"
+
+# remove < and > from 'val' column
+df$val = as.numeric(sub("^<|>", "", df$val))
+
+# drop zeros; lab meant to exclude these
+df <- df[df$val > 0, ]
+
+# calculate mean titers using tobit as needed
+titers = c()
+
+for(iso in levels(df$isolate)){ #isolates/pubids map 1:1
+  for(mab in levels(df$mab_name)){
+    for(t in c("TITER_80_CURVE", "TITER_50_CURVE")){
+      # subset to one isolate-mab pair and either IC50 or 80
+      ss = subset(df, isolate==iso & mab_name==mab & value_label==t)
+      
+      if(all(ss$lod=="")){
+        # all-numeric case
+        titers = rbind(
+          titers,
+          data.frame(
+            isolate=iso,
+            mab_name=mab,
+            pct_neut=t,
+            titer=geometric.mean(ss$val),
+            case="all numeric",
+            n_numeric=nrow(ss),
+            n_above=0,
+            n_below=0
+          )
+        )
+      } else if(all(ss$lod==">")){
+        # all above upper threshold case
+        titers = rbind(
+          titers,
+          data.frame(
+            isolate=iso,
+            mab_name=mab,
+            pct_neut=t,
+            titer=">25",
+            case="all above",
+            n_numeric=0,
+            n_above=nrow(ss),
+            n_below=0
+          )
+        )
+      } else if(all(ss$lod=="<")){
+        # all below lower threshold case
+        titers = rbind(
+          titers,
+          data.frame(
+            isolate=iso,
+            mab_name=mab,
+            pct_neut=t,
+            titer="<0.011431184270690446",
+            case="all below",
+            n_numeric=0,
+            n_above=0,
+            n_below=nrow(ss)
+          )
+        )
+      } else if(any(ss$lod==">") && !any(ss$lod=="<")){
+        # some above threshold, some numeric, none below
+        n_above <- sum(ss$lod == ">")
+        ss$surv <- Surv(log(ss$val), ifelse(ss$lod==">", 0, 1))
+        fit = survreg(ss$surv~1, dist="gaussian")
+        mu = exp(coef(fit))
+        titers = rbind(
+          titers,
+          data.frame(
+            isolate=iso,
+            mab_name=mab,
+            pct_neut=t,
+            titer=mu,
+            case="some above",
+            n_numeric=nrow(ss)-n_above,
+            n_above=n_above,
+            n_below=0
+          )
+        )
+      } else if (any(ss$lod=="<") && !any(ss$lod==">")){
+        # some above threshold, some numeric, none below
+        n_below <- sum(ss$lod == "<")
+        ss$surv <- Surv(log(ss$val), ifelse(ss$lod=="<", 0, 1))
+        fit = survreg(ss$surv~1, dist="gaussian")
+        mu = exp(coef(fit))
+        titers = rbind(
+          titers,
+          data.frame(
+            isolate=iso,
+            mab_name=mab,
+            pct_neut=t,
+            titer=mu,
+            case="some below",
+            n_numeric=nrow(ss)-n_below,
+            n_above=0,
+            n_below=n_below
+          )
+        )
+      } else {
+        # if any rows have both above and below
+        n_above <- sum(ss$lod == ">")
+        n_below <- sum(ss$lod == "<")
+        titers = rbind(
+          titers,
+          data.frame(
+            isolate=iso,
+            mab_name=mab,
+            pct_neut=t,
+            titer="CAUTION, BOTH ABOVE AND BELOW",
+            case="CAUTION, BOTH ABOVE AND BELOW",
+            n_numeric=nrow(ss)-n_above-n_below,
+            n_above=n_above,
+            n_below=n_below
+          )
+        )
+      }
+    }
+  }
+}
+
+savedir <- "N:/vtn/lab/SDMC_labscience/studies/VISC/Ochsenbauer_914/assays/C-C_nAb/misc_files/interim_data/"
+today <- as.character(Sys.Date())
+
+write.table(
+  titers,
+  paste0(savedir, "tobit_titers_", today, ".txt"),
+  sep='\t',
+  row.names=FALSE,
+  quote = FALSE
+)
+
---
+++ processing_misc/CAVD914/Oschenbauer_CC_nAb/reformat_ccnab_data.py
@@ -0,0 +1,114 @@
+## ------------------------------------------------------------------------------------------##
+# Author: Beatrix Haddock
+# Date: 2025-11-14
+# Purpose: Reformat IC50/80 estimates from R script
+## ------------------------------------------------------------------------------------------##
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+
+titer_estimates = pd.read_csv("/networks/vtn/lab/SDMC_labscience/studies/VISC/Ochsenbauer_914/assays/C-C_nAb/misc_files/interim_data/tobit_titers_2025-11-13.txt", sep='\t')
+
+titer_estimates['titer_numeric'] = titer_estimates.titer.str.replace(">","").str.replace("<","").astype(float)
+titer_estimates['titer_recensored'] = titer_estimates.titer.copy()
+titer_estimates.loc[titer_estimates.titer_numeric > 25, 'titer_recensored'] = ">25"
+titer_estimates.loc[titer_estimates.titer_numeric < 0.011431184270690446, 'titer_recensored'] = '<0.011431184270690446'
+
+# ensure only one estimate per isolate/mab/titer
+assert len(titer_estimates.drop_duplicates().groupby(['isolate','mab_name','pct_neut']).titer.count().value_counts()) == 1
+
+titer_estimates = titer_estimates.sort_values(by=['isolate','mab_name','pct_neut']).drop_duplicates()
+titer_estimates.pct_neut = titer_estimates.pct_neut.map({'TITER_50_CURVE':'IC50','TITER_80_CURVE':'IC80'})
+
+titer_estimates['n_replicates'] = titer_estimates.n_numeric + titer_estimates.n_above + titer_estimates.n_below
+titer_estimates['n_outside_cutoff'] = titer_estimates.n_above + titer_estimates.n_below
+
+# make sure same number of replicates for ic50 and ic80
+tmp = titer_estimates.pivot(
+    index=['isolate','mab_name'],
+    columns='pct_neut',
+    values='n_replicates'
+)
+assert (tmp.IC50 != tmp.IC80).sum() == 0
+
+recensored_wide = titer_estimates.pivot(
+    index=['isolate','mab_name'],
+    columns='pct_neut',
+    values='titer_recensored'
+)
+
+precensored_wide = titer_estimates.pivot(
+    index=['isolate','mab_name'],
+    columns='pct_neut',
+    values='titer'
+)
+
+outside_cutoff = titer_estimates.pivot(
+    index=['isolate','mab_name'],
+    columns='pct_neut',
+    values='n_outside_cutoff'
+).rename(columns={'IC50':'n_outside_cutoff_IC50','IC80':'n_outside_cutoff_IC80'})
+
+estimates_wide = recensored_wide.merge(precensored_wide, left_index=True, right_index=True, suffixes=('_recensored','_tobit_fit'))
+estimates_wide = estimates_wide.merge(outside_cutoff, left_index=True, right_index=True)
+
+n_reps = titer_estimates.set_index(['isolate','mab_name'])[['n_replicates']].reset_index().drop_duplicates().set_index(['isolate','mab_name'])
+
+estimates_wide = estimates_wide.merge(
+    n_reps, 
+    left_index=True, right_index=True
+)
+
+estimates_wide['n_replicates_ic50_numeric'] = estimates_wide.n_replicates - estimates_wide.n_outside_cutoff_IC50
+estimates_wide['n_replicates_ic80_numeric'] = estimates_wide.n_replicates - estimates_wide.n_outside_cutoff_IC80
+
+estimates_wide.columns = [i.lower() for i in estimates_wide.columns]
+
+rename = {
+    'ic50_recensored': 'ic50_recensored',
+    'ic80_recensored': 'ic80_recensored',
+    'ic50_tobit_fit': 'ic50',
+    'ic80_tobit_fit': 'ic80',
+    'n_outside_cutoff_ic50': 'n_outside_cutoff_ic50',
+    'n_outside_cutoff_ic80': 'n_outside_cutoff_ic80',
+    'n_replicates': 'n_replicates',
+    'n_replicates_ic50_numeric': 'n_replicates_ic50_numeric',
+    'n_replicates_ic80_numeric': 'n_replicates_ic80_numeric'
+}
+estimates_wide = estimates_wide.rename(columns=rename)
+
+final = estimates_wide[['n_replicates','ic50','ic50_recensored','n_replicates_ic50_numeric','ic80','ic80_recensored','n_replicates_ic80_numeric']].reset_index()
+
+# read in data with IC50/80 estimates to get pubid back
+df = pd.read_csv('/networks/cavd/Studies/cvd914/qdata/LabData/NAB/CAVD914_NAB07_U6_20250924.txt', sep='\t')
+df.columns = [i.lower() for i in df.columns]
+df = df.rename(columns={
+    'isolate':'tetO_HIV_Env_IMC_name',
+    'guspec':'mab_name',
+    'value':'val',
+})
+df['isolate'] = df.tetO_HIV_Env_IMC_name.str.split(".", expand=True)[1].str[1:]
+
+pubids = df.isolate.str.split("_", expand=True).iloc[:,:2]
+df['pubid'] = pubids[0]+"-"+pubids[1]
+
+# these should uniquely define one another
+assert df[['pubid','isolate']].drop_duplicates().shape[0] == df.pubid.nunique()
+assert df.pubid.nunique() == df.isolate.nunique()
+
+df[['isolate','pubid']].drop_duplicates()
+
+final = final.merge(df[['pubid','isolate']].drop_duplicates(), how='left', on='isolate')
+
+final = final[['isolate','mab_name','pubid','n_replicates','ic50','ic50_recensored','n_replicates_ic50_numeric','ic80','ic80_recensored','n_replicates_ic80_numeric']]
+
+cc_dir = "/networks/vtn/lab/SDMC_labscience/studies/VISC/Ochsenbauer_914/assays/C-C_nAb/misc_files/"
+final.to_csv(cc_dir + "data_processing/CAVD914_CC_nAb_IC50_80_2025-11-14.txt", sep="\t", index=False)
+
+ic50_numeric = final.ic50.str.replace(">","").str.replace("<","").astype(float)
+ic80_numeric = final.ic80.str.replace(">","").str.replace("<","").astype(float)
+
+# visual check on outliers
+final.loc[(ic50_numeric > ic80_numeric)]
