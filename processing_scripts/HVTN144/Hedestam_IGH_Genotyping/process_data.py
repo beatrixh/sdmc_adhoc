31351baa72655d42fec0c0f769e94558e7e61572
Beatrix Haddock <bhaddock@statsrv.pc.scharp.org>
2026-02-27 16:30:48 -0800

    updating with new novel alleles/corresponding sequences

--- processing_scripts/HVTN144/Hedestam_IGH_Genotyping/process_data.py
+++ processing_scripts/HVTN144/Hedestam_IGH_Genotyping/process_data.py
@@ -1,6 +1,7 @@
 ## ---------------------------------------------------------------------------##
 # Author: Beatrix Haddock
 # Date: 09/26/2025
+# Updated: 02/27/2026
 # Purpose: Process Ig Genotyping data from Hedestam Lab (Karolinka)
 ## ---------------------------------------------------------------------------##
 import pandas as pd
@@ -11,9 +12,9 @@ import datetime as datetime
 import sdmc_tools.process as sdmc_tools
 import sdmc_tools.access_ldms as access_ldms
 
-def main():
+# def main():
     datadir = '/trials/vaccine/p144/s001/qdata/LabData/genotyping_pass-through/'
-
+    
     # cast to long
     def format_data(excel_dict):
         data = []
@@ -29,40 +30,40 @@ def main():
             d['region'] = k
             d['gene'] = d.allele.str.split('*', expand=True)[0]
             data += [d]
-
+    
         data = pd.concat(data)
         data = data.loc[data.allele.notna()]
-
+    
         # cast to wide on allele 1/2/3/4, leave 'na' for ptid/genes with < 4 alleles
         data['allele_id'] = 1
         data['allele_id'] = 'allele_' + data.groupby(['ptid','gene']).allele_id.transform('cumsum').astype(str)
-
+    
         data = pd.pivot(data, index=['ptid','region','gene'], columns='allele_id', values='allele')
         data = data.reset_index()
         data.columns.name = ''
         return data
-
+    
     datadir = '/trials/vaccine/p144/s001/qdata/LabData/genotyping_pass-through/'
     fnames = [i for i in os.listdir(datadir) if '.xlsx' in i and '$' not in i]
     datasets = [pd.read_excel(datadir + i, sheet_name=None) for i in fnames]
-
+    
     data = pd.concat([format_data(d) for d in datasets])
-
+    
     mfst1 = pd.read_csv('/networks/vtn/lab/SDMC_labscience/studies/HVTN/HVTN144/assays/genotyping/misc_files/512--2024000249.txt', sep="\t")
     mfst2 = pd.read_csv('/networks/vtn/lab/SDMC_labscience/studies/HVTN/HVTN144/assays/genotyping/misc_files/512--2025000058.txt', sep="\t")
-
-
+    
+    
     mfst = pd.concat([mfst1, mfst2])
     # set(mfst.PID).symmetric_difference(data.ptid)
     # mfst.PID.nunique(), len(mfst)
-
+    
     data['guspec'] = data.ptid.map(mfst.set_index('PID').GLOBAL_ID)
-
+    
     ldms = access_ldms.pull_one_protocol('hvtn', 144)
     ldms = ldms.loc[ldms.guspec.isin(data.guspec)]
-
+    
     # set(ldms.guspec).symmetric_difference(data.guspec)
-
+    
     # standard processing
     md = {
         'network':'HVTN',
@@ -74,7 +75,7 @@ def main():
         'lab_software':'IgDiscover',
         'lab_software_version':'IgDiscover22 v1.0.5.',
     }
-
+    
     outputs = sdmc_tools.standard_processing(
         input_data=data.drop(columns='ptid'),
         input_data_path=datadir,
@@ -83,9 +84,9 @@ def main():
         metadata_dict=md,
         ldms=ldms
     )
-
+    
     outputs['input_file_name'] = 'GENOTYPES_IG'+outputs.region.str[2]+'_FILT.xlsx'
-
+    
     # reorder columns
     reorder = [
         'network',
@@ -116,14 +117,14 @@ def main():
         'input_file_name'
     ]
     assert len(set(reorder).symmetric_difference(outputs.columns)) == 0
-
+    
     outputs = outputs[reorder]
-
+    
     # save to .txt
     savedir = '/networks/vtn/lab/SDMC_labscience/studies/HVTN/HVTN144/assays/genotyping/misc_files/data_processing/'
     today = datetime.date.today().isoformat()
     outputs.to_csv(savedir + f"HVTN144_Ig_Genotyping_processed_{today}.txt", sep="\t", index=False)
-
+    
     # pivot summaries
     pivot_summary = pd.melt(
         outputs,
@@ -131,9 +132,9 @@ def main():
         value_vars=['allele_1','allele_2','allele_3','allele_4'],
         value_name='allele'
     )
-
+    
     pivot_summary = pivot_summary.loc[pivot_summary.allele.notna()]
-
+    
     summary2 = pd.pivot_table(
         pivot_summary,
         index=['guspec','ptid'],
@@ -141,7 +142,7 @@ def main():
         aggfunc='count',
         fill_value=0
     )[['allele']]
-
+    
     pivot_summary['category'] = pivot_summary.region.str[2]
     summary0 = pd.pivot_table(
         pivot_summary,
@@ -150,40 +151,68 @@ def main():
         aggfunc='count',
         fill_value=0
     )['allele']
-
-
-
+    
     summary0.to_excel(savedir + "HVTN144_Ig_Genotyping_region_summary.xlsx")
     summary2.to_excel(savedir + "HVTN144_Ig_Genotyping_visitno_summary_.xlsx")
-
-
+    
+    
     # save novel allele sequences to fasta file
     novels_h = pd.read_excel(datadir + 'GENOTYPES_IGH_FILT.xlsx', sheet_name='Novel_alleles').rename(columns={'IGHV':0})
     novels_k = pd.read_excel(datadir + 'GENOTYPES_IGK_FILT.xlsx', sheet_name='IGKV_novel_alleles', header=None)
     novels_l = pd.read_excel(datadir + 'GENOTYPES_IGL_FILT.xlsx', sheet_name='IGLV_Novel_Alleles', header=None)
-
+    
     # drop the header rows
     novels_h = novels_h.loc[novels_h[0].str[:3]!="IGH"]
     novels = pd.concat([novels_h, novels_k, novels_l])
+        
+    # updated alleles identified by ju, sequences from the lab
+    additional = pd.read_excel(
+        "/networks/vtn/lab/SDMC_labscience/studies/HVTN/HVTN144/assays/genotyping/misc_files/additional_novel_alleles.xlsx",
+        sheet_name=None
+    )
+        
+    additional_144 = ">" + additional['alleles in 144']['alleles in 144'] + "\n" + additional['alleles in 144']['sequence']
+    additional_144 = additional_144.str.cat(sep="\n")
 
+    # these will go in their own file
+    # additional_301 = ">" + additional['alleles in 301']['alleles in 301'] + "\n" + additional['alleles in 301']['sequence']
+    # additional_301 = additional_301.str.cat(sep="\n")
+    
     fasta = novels[0].str.cat(sep="\n")
-
+    fasta = fasta + "\n" + additional_144 # + "\n" + additional_301
+    
+    # check against the data
+    alleles = outputs.melt(
+        value_vars=[    
+            'allele_1',
+            'allele_2',
+            'allele_3',
+            'allele_4',
+        ],
+        value_name='allele'
+    )['allele'].dropna()
+    alleles = alleles.loc[alleles.str.contains("_S")]
+    
+    in_fasta = [i[1:] for i in fasta.split("\n") if ">" in i]
+    
+    assert set(alleles).symmetric_difference(in_fasta) == set()
+    
     with open(savedir + f"HVTN144_genotyping_novel_allele_sequences_{today}.fasta", "w", encoding="utf-8") as f:
         f.write(fasta)
-
+    
     ## save deletion list to .txt
     dels = pd.read_excel(datadir + 'GENOTYPES_IGH_FILT.xlsx', sheet_name='D_region_deletion')
     deletions = pd.DataFrame(data={
         'ptid': dels['Cases with probable hemizygous_deletion of IGHD region '].iloc[:13].tolist(),
         'comment': ['Heterozygous for a SNP associated with the contiguous deletion of the six IGHD genes between IGHD2-2 to IGHD3-9']*13
     })
-
+    
     non_dels = list(set(outputs.ptid).difference(deletions.ptid))
     non_deletions = pd.DataFrame(data = {
         'ptid': non_dels,
         'comment': ["Homozygous for a non-deletion variant of the six IGHD genes between IGHD2-2 to IGHD3-9"]*len(non_dels)
     })
-
+    
     possible_deletions = pd.concat([deletions, non_deletions]).reset_index(drop=True)
     possible_deletions.ptid = possible_deletions.ptid.astype(int)
     possible_deletions['guspec'] = possible_deletions.ptid.map(mfst.set_index('PID').GLOBAL_ID)
