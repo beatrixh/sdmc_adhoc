e3601d41f03871ee4df5a4c1d92ac70605549f44
Beatrix Haddock <bhaddock@statsrv.pc.scharp.org>
2026-03-18 17:49:35 -0700

    update for actual input files

--- processing_scripts/CoVPN3008/MTB_Rif/process_data_combined.py
+++ processing_scripts/CoVPN3008/MTB_Rif/process_data_combined.py
@@ -1,125 +1,52 @@
+## ---------------------------------------------------------------------------##
+# Author: Beatrix Haddock
+# Date: 03/18/2026
+# Purpose: Process MTB Rif simple machine outputs/converted pdfs 
+##         and merge on complex machine outputs
+## ---------------------------------------------------------------------------##
 import pandas as pd
 import numpy as np
 import sdmc_tools.access_ldms as access_ldms
 import sdmc_tools.process as sdmc
 import os
 import datetime
-
 from io import StringIO
 
-
+## read in data, standard formatting/processing, merge on complex outputs / save
 def main():
-    ## parse pdf data ---------------------------------------------------------------------------- ##
-    def parse_pdf(pdf_path):
-        data = pd.read_excel(
-            pdf_path,
-            header=None
-        )
-        
-        metadata = {}
-        blocks = []
-        # blocks += [data.iloc[0].dropna().tolist()]
-        
-        metadata['sample_id'] = data.iloc[0].dropna().tolist()[1].split("\n")[0]
-        
-        # blocks += [data.iloc[2:4].dropna(how='all', axis=1)]
-        tmp = data.iloc[2:4].dropna(how='all', axis=1)
-        metadata_new = dict(zip(tmp.iloc[0], tmp.iloc[1]))
-        metadata = metadata | metadata_new
-        
-        # blocks += [data.iloc[5].dropna().tolist()]
-        # metadata['result_mtb_interpretation'] = data.iloc[5].dropna().tolist()[1]
-        
-        idx_start = data[data.apply(lambda row: row.astype(str).str.replace(" ","").str.lower().str.contains("user:status").any(), axis=1)].index[0]
-        idx_end = data[data.apply(lambda row: row.astype(str).str.replace(" ","").str.lower().str.contains("reagentlotid").any(), axis=1)].index[0]
-        tmp = data.iloc[idx_start:idx_end+1].dropna(how='all', axis=1)
-        tmp = pd.concat([split_row(row) for _, row in tmp.iterrows()],
-                      ignore_index=True)
-        tmp.columns = ['a','b','a','b']
-        tmp = pd.concat([
-            tmp.iloc[:,[0,1]],
-            tmp.iloc[:,[2,3]]
-        ]).dropna(how='all')
-        
-        metadata_new = dict(zip(tmp.iloc[:,0], tmp.iloc[:,1]))
-        metadata = metadata | metadata_new
-
-        idx_start = data[data.apply(lambda row: row.astype(str).str.replace(" ","").str.lower().str.contains("analytect").any(), axis=1)].index[0]
-        idx_end = data[data.apply(lambda row: row.astype(str).str.replace(" ","").str.lower().str.contains("user:status:").any(), axis=1)].index[0]
-        df = data.iloc[idx_start+1:idx_end].dropna(how='all', axis=1)
-        df.columns = ['analyte','ct','endpoint','analyte_result','result_probe_control']
-        df = pd.concat([split_row(row) for _, row in df.iterrows()],
-                      ignore_index=True)
-        df = df.merge(pd.DataFrame([metadata]), how='cross')
-        disclaimer = data[data.apply(lambda row: row.astype(str).str.replace(" ","").str.lower().str.contains("diagnostic").any(), axis=1)].values[-1][0]
-        df['test_disclaimer'] = disclaimer
-        column_rename = {
-            'analyte': 'analyte_name',
-            # 'ct': 'ct',
-            'endpoint': 'endpt',
-            'analyte_result': 'result_qualitative',
-            # 'result_probe_control': 'result_probe_control',
-            # 'sample_id': 'sample_id',
-            # 'Assay': 'Assay',
-            'Assay Version': 'assay_version',
-            'Assay Type': 'assay_categorization_lab',
-            'User: Status:': 'user_status',
-            'Error Status:':'error_status',
-            'Reagent Lot ID*: Notes:':'reagent_lot_id',
-            'Expiration Date*:': 'reagent_lot_expiration',
-            'S/W Version:': 'lab_software_version',
-            'Cartridge S/N*:': 'cartridge_serial',
-            'Start Time:': 'start_time',
-            'End Time:': 'end_time',
-            'Instrument S/N:': 'instrument_serial',
-            'Module S/N:': 'module_serial',
-            'Module Name:':'module_name',
-        }
-        df = df.rename(columns=column_rename)
-        df.columns = [i.lower().replace(" ","_") for i in df.columns]
-        dropcols = [
-            'assay', 'assay_name','test_type'
-        ]
-        dropcols = list(set(dropcols).intersection(df.columns))
-        df = df.drop(columns=dropcols)
-        df = pd.concat([
-            df.drop(columns='user_status'),
-            df.user_status.str.split(" ", expand=True).rename(columns={0:'user',1:'status'})
-        ], axis=1)
-        return df
-
-    ## @sara this is where the real pdf filepaths should go!
-    pdf_dir = '/networks/vtn/lab/SDMC_labscience/assays/MTB-Rif/example_data/'
+    ## read in data
+    pdf_dir = '/trials/covpn/p3008t/s001/qdata/LabData/MTB-Rif_pass-through/'
     pdf_data = [
-        'pdf_conversion_123456789V302_2025.12.22_10.26.49_details.xlsx',
-        'pdf_conversion_297800187V302_2025.12.22_10.26.49_details.xlsx',
-        'pdf_conversion_339800837V302_2025.12.22_12.01.19_details.xlsx',
+        '784801133V302.xlsx',
+        '792801574V302.xlsx',
+        '807803673V302.xlsx',
+        '816802568V302.xlsx',
+        '836803289V302.xlsx',
+        '836803582V302.xlsx',
     ]
-
+    
     pdfs = pd.concat([parse_pdf(pdf_dir + p) for p in pdf_data])
-
-    ## @sara below i mapped the pdf ids to ones that are actually in the data to make the fake data look real.
-    ## you should delete this and then check that the merge worked well -- ill @ you below
-    # PRETENDING THESE ARE REAL IDS THAT WE WANTED! THIS IS CREATING FAKE DATA
-    # THE REAL DATA SHOULD ALREADY HAVE MATCHING IDS
-    pdfs = pdfs.loc[pdfs.sample_id!='339800837V302']
-    pdfs.sample_id = pdfs.sample_id.map({
-        '123456789V302':'792801574V302', 
-        '297800187V302':'784801133V302',
-    })
-
+    
     ## read in simple outputs ---------------------------------------------------------------------------- ##
     datadir = '/trials/covpn/p3008t/s001/qdata/LabData/MTB-Rif_pass-through/'
-
+    
     input_data_path1 = '/trials/covpn/p3008t/s001/qdata/LabData/MTB-Rif_pass-through/66_Tests_Exported_2026.01.20_13.21.27.csv'
     df1 = pd.read_csv(input_data_path1)
-
+    
     input_data_path2 = '/trials/covpn/p3008t/s001/qdata/LabData/MTB-Rif_pass-through/68_Tests_Exported_2026.01.20_13.29.00.csv'
     df2 = pd.read_csv(input_data_path2)
-
+    
+    # save which rows map to which file
+    input_file_map = {i:'66_Tests_Exported_2026.01.20_13.21.27.csv' for i in df1['Patient ID']}
+    input_file_map = input_file_map | {i:'68_Tests_Exported_2026.01.20_13.29.00.csv' for i in df2['Patient ID']}
+    
+    sdmc_data_receipt_time_map1 = {i:datetime.datetime.fromtimestamp(os.path.getmtime(input_data_path1)).isoformat() for i in df1['Patient ID']}
+    sdmc_data_receipt_time_map2 = {i:datetime.datetime.fromtimestamp(os.path.getmtime(input_data_path2)).isoformat() for i in df2['Patient ID']}
+    sdmc_data_receipt_time_map = sdmc_data_receipt_time_map1 | sdmc_data_receipt_time_map2
+    
     df1 = df1.dropna(how='all')
     df2 = df2.dropna(how='all')
-
+    
     df = pd.concat([df1, df2])
     df = pd.concat([
         df.drop(columns='Result'),
@@ -132,19 +59,41 @@ def main():
         'patient_id':'sample_id'
     })
     df = df.drop(columns=['assay_name'])
-
+    
     df.reagent_lot_id = df.reagent_lot_id.astype(float)
     pdfs.reagent_lot_id = pdfs.reagent_lot_id.astype(float)
+    
+    set(df.columns).intersection(pdfs.columns)
+    
     tmp = df.merge(pdfs, on=['sample_id','reagent_lot_id'], how='left')
-
-    # @sara ive just added this simple check to ensure the merge worked correctly. might want to check this more closely
+    
     # THE MERGE SHOULD ADD ROWS FOR THE PTIDS IN THE PDFS
     assert len(df) - pdfs.sample_id.nunique() + len(pdfs) == len(tmp)
     df = df.merge(pdfs, on=['sample_id','reagent_lot_id'], how='left')
-
+    
+    df.head()
+    
+    # Merge on guspecs
+    guspecs = pd.read_csv(datadir + '3008_Global specs.csv')
+    
+    guspecs = guspecs.rename(columns={'Global Unique Id':'guspec'})
+    guspecs = guspecs.dropna(how='all')
+    
+    df['ptid'] = df.sample_id.str[:-4]
+    df.ptid = df.ptid.astype(int)
+    guspecs.ptid = guspecs.ptid.astype(int)
+    
+    tmp = df.merge(guspecs[['guspec','ptid','visitno']], on='ptid', how='left')
+    assert len(tmp) == len(df)
+    df = df.merge(guspecs[['guspec','ptid','visitno']], on='ptid', how='left')
+    
+    df[['sample_id','ptid','guspec']].nunique()
+    
+    df[['sample_id','ptid','guspec']].drop_duplicates().shape
+    
     ## standard processing ------------------------------------------------------------------------------- ##
     ldms = access_ldms.pull_one_protocol('covpn', 3008)
-
+    
     md = {
         'network': 'CoVPN',
         'protocol': 3008,
@@ -159,37 +108,212 @@ def main():
         'assay_precision': 'Semi-Quantitative',
         'sdmc_pipeline_version':'Complex',
     }
-
+    
     # @sara the real guspecs should be merged on here!
     # @sara in addition the real filepaths to the input data need to be merged onto each row - im sorry i didnt get to this
-    df['FAKE_PLACEHOLDER_GUSPEC'] = '0666-004MZG00-001'
     outputs = sdmc.standard_processing(
         input_data=df,
         input_data_path='FILL THIS IN',
-        guspec_col="FAKE_PLACEHOLDER_GUSPEC",
+        guspec_col='guspec',
         network="CoVPN",
         metadata_dict=md,
         ldms=ldms
     )
-
+    
+    set(outputs.sample_id).difference(input_file_map.keys())
+    
+    outputs['input_file_name'] = outputs.sample_id.map(input_file_map)
+    outputs['sdmc_data_receipt_datetime'] = outputs.sample_id.map(sdmc_data_receipt_time_map)
+    
+    outputs.ptid_x = outputs.ptid_x.astype(int)
+    outputs.ptid_y = outputs.ptid_y.astype(int)
+    assert (outputs.ptid_x!=outputs.ptid_y).sum() == 0
+    
+    outputs.visitno_x = outputs.visitno_x.astype(float)
+    outputs.visitno_y = outputs.visitno_y.astype(float)
+    assert (outputs.visitno_x!=outputs.visitno_y).sum() == 1 #0332-02PJVB00-001 has a weird value in the input file
+    
+    outputs = outputs.drop(columns=['ptid_x','visitno_x'])
+    outputs = outputs.rename(columns={'ptid_y':'ptid', 'visitno_y':'visitno'})
+    
     ## merge on complex outputs ---------------------------------------------------------------------------- ##
-
     complex_outputs = '/networks/vtn/lab/SDMC_labscience/studies/CoVPN/CoVPN3008/assays/MTB-Rif/misc_files/data_processing/DRAFT_MTB_Rif_complex_output_processed_2026-03-06.txt'
     complex_outputs = pd.read_csv(complex_outputs, sep='\t')
-    complex_outputs['pipeline_version'] = 'Simple'
-    outputs = pd.concat([complex_outputs, outputs.drop(columns='fake_placeholder_guspec')])
+    complex_outputs['sdmc_pipeline_version'] = 'Simple'
+    
+    complex_outputs = complex_outputs.rename(columns={
+        'analyte_result':'result_qualitative',
+        'result_mtb':'result_mtb_interpretation',
+        'result_rif':'result_rif_interpretation',
+        'cartridge_s/n':'cartridge_serial',
+        'module_s/n':'module_serial',
+        'instrument_s/n':'instrument_serial',
+        'probe_check_result':'result_probe_control',
+        'expiration_date':'reagent_lot_expiration',
+    })
 
+    # these contain almost the same columns -- no cartridge_index in the simple machine outputs
+    # set(reorder).difference(complex_outputs.columns)
+    # set(complex_outputs.columns).difference(reorder)
+    # set(complex_outputs.columns).difference(outputs.columns)
+    
+    reorder = [
+        'network',
+        'protocol',
+        'specrole',
+        'guspec',
+        'ptid',
+        'visitno',
+        'drawdt',
+        'spectype',
+        'spec_primary',
+        'spec_additive',
+        'spec_derivative',
+        'upload_lab_id',
+        'assay_lab_name',
+        'assay_type',
+        'assay_subtype',
+        'assay_kit',
+        'assay_precision',
+        'instrument',
+        'instrument_serial',
+        'lab_software',
+        'lab_software_version',
+        'sdmc_pipeline_version',
+        'analyte_name',
+        'assay_categorization_lab',
+        'assay_version',
+        'cartridge_index',
+        'cartridge_serial',
+        'ct',
+        'end_time',
+        'endpt',
+        'error_status',
+        'lab_internal_sample_id_date',
+        'module_name',
+        'module_serial',
+        'reagent_lot_id',
+        'reagent_lot_expiration',
+        'result_qualitative',
+        'result_mtb_interpretation',
+        'result_rif_interpretation',
+        'result_probe_control',
+        'sample_id',
+        'start_date_&_time',
+        'start_time',
+        'status',
+        'test_disclaimer',
+        'test_type',
+        'user',
+        'sdmc_processing_datetime',
+        'sdmc_data_receipt_datetime',
+        'input_file_name',
+        'input_file_converted_pdf',
+    ]
+    
+    outputs = pd.concat([complex_outputs, outputs])
+    
     # @sara might want to reorder columns
-
+    assert set(reorder).symmetric_difference(outputs.columns) == set()
+    outputs = outputs[reorder]
+    
     ## save ----------------------------------------------------------------------------------------------- ##
     today = datetime.date.today().isoformat()
     savedir = "/networks/vtn/lab/SDMC_labscience/studies/CoVPN/CoVPN3008/assays/MTB-Rif/misc_files/data_processing/"
-    fname=f"DRAFT_FAKE_MTB_Rif_combined_output_processed_{today}.txt"
-
+    fname=f"DRAFT_MTB_Rif_combined_output_processed_{today}.txt"
+    
     outputs.to_csv(
         savedir + fname, sep='\t', index=False
     )
 
+
+## fn to parse pdf data ---------------------------------------------------------------------------- ##
+def parse_pdf(pdf_path):
+    data = pd.read_excel(
+        pdf_path,
+        header=None
+    )
+    
+    metadata = {}
+    blocks = []
+    
+    metadata['sample_id'] = data.iloc[0].dropna().tolist()[1].split("\n")[0]
+    
+    tmp = data.iloc[2:4].dropna(how='all', axis=1)
+    metadata_new = dict(zip(tmp.iloc[0], tmp.iloc[1]))
+    metadata = metadata | metadata_new
+        
+    data.loc[data[0]=="Reagent Lot*: Error Status:", 0] = "Reagent Lot*:\nError Status:"
+    idx_start = data[(
+        (data.apply(lambda row: row.astype(str).str.replace(" ","").str.lower().str.contains("user:").any(), axis=1)) &
+        (data.apply(lambda row: row.astype(str).str.replace(" ","").str.lower().str.contains("status").any(), axis=1))
+    )].index[0]
+    idx_end = data[data.apply(lambda row: row.astype(str).str.replace(" ","").str.lower().str.contains("reagentlot").any(), axis=1)].index[0]
+    tmp = data.iloc[idx_start:idx_end+1].dropna(how='all', axis=1)
+    tmp = pd.concat([split_row(row) for _, row in tmp.iterrows()],
+                  ignore_index=True)
+    tmp.columns = ['a','b','a','b']
+    tmp = pd.concat([
+        tmp.iloc[:,[0,1]],
+        tmp.iloc[:,[2,3]]
+    ]).dropna(how='all')
+    metadata_new = dict(zip(tmp.iloc[:,0], tmp.iloc[:,1]))
+    metadata = metadata | metadata_new
+           
+    idx_start = data[data.apply(lambda row: row.astype(str).str.replace(" ","").str.lower().str.contains("analytect").any(), axis=1)].index[0]
+    idx_end = data[(
+        (data.apply(lambda row: row.astype(str).str.replace(" ","").str.lower().str.contains("user:").any(), axis=1)) &
+        (data.apply(lambda row: row.astype(str).str.replace(" ","").str.lower().str.contains("status").any(), axis=1))
+    )].index[0]
+    df = data.iloc[idx_start+1:idx_end].dropna(how='all', axis=1)
+    df.columns = ['analyte','ct','endpoint','analyte_result','result_probe_control']
+    df = pd.concat([split_row(row) for _, row in df.iterrows()],
+                  ignore_index=True)
+    
+    df = df.merge(pd.DataFrame([metadata]), how='cross')
+    disclaimer = data[data.apply(lambda row: row.astype(str).str.replace(" ","").str.lower().str.contains("diagnostic").any(), axis=1)].values[-1][0]
+    df['test_disclaimer'] = disclaimer
+    df.columns = [i.replace("*",'') for i in df.columns]
+    column_rename = {
+        'analyte': 'analyte_name',
+        # 'ct': 'ct',
+        'endpoint': 'endpt',
+        'analyte_result': 'result_qualitative',
+        # 'result_probe_control': 'result_probe_control',
+        # 'sample_id': 'sample_id',
+        # 'Assay': 'Assay',
+        'Assay Name':'assay_name',
+        'Assay Version': 'assay_version',
+        'Assay Type': 'assay_categorization_lab',
+        'User: Status:': 'user_status',
+        'User:':'user',
+        'Status:':'status',
+        'Error Status:':'error_status',
+        'Reagent Lot ID: Notes:':'reagent_lot_id',
+        'Reagent Lot:':'reagent_lot_id',
+        'Expiration Date:': 'reagent_lot_expiration',
+        'S/W Version:': 'lab_software_version',
+        'Cartridge S/N:': 'cartridge_serial',
+        'Start Time:': 'start_time',
+        'End Time:': 'end_time',
+        'Instrument S/N:': 'instrument_serial',
+        'Module S/N:': 'module_serial',
+        'Module Name:':'module_name',
+    }
+    df = df.rename(columns=column_rename)
+    df.columns = [i.lower().replace(" ","_") for i in df.columns]
+    dropcols = [
+        'assay', 'assay_name','test_type'
+    ]
+    dropcols = list(set(dropcols).intersection(df.columns))
+    df = df.drop(columns=dropcols)
+    if 'user_status' in df.columns:
+        df = pd.concat([
+            df.drop(columns='user_status'),
+            df.user_status.str.split(" ", expand=True).rename(columns={0:'user',1:'status'})
+        ], axis=1)
+    df['input_file_converted_pdf'] = pdf_path.split("/")[-1]
+    return df
 
 # helper fn for rows of data that got combined into one excel cell separated by a "\n"
 def split_row(row):
@@ -204,6 +328,7 @@ def split_row(row):
     
     # Convert to DataFrame
     return pd.DataFrame(dict(zip(row.index, split_cols)))
+
 
 
 if __name__=="__main__":
