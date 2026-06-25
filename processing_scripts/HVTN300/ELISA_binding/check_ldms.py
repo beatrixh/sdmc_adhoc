6976ebfffd49aebc27c1490f7e2dcf2ae7b9e623
Sara Thiebaud <sthiebau@statsrv.pc.scharp.org>
2026-06-25 12:12:41 -0700

    Initial commit for HVTN 300 and 307 Haynes lab binding and blocking code (LDMS checks only). HVTN 307 files will need to be updated (specifically column names)  when we receive the full versions of the datasets with guspec.

---
+++ processing_scripts/HVTN300/ELISA_binding/check_ldms.py
@@ -0,0 +1,60 @@
+## ---------------------------------------------------------------------------##
+# Author: Sara Thiebaud
+# Date: 24 June 2026
+# Purpose: HVTN 300 binding ELISA data from the Haynes lab; LDMS checks only
+## ---------------------------------------------------------------------------##
+
+import pandas as pd
+import numpy as np
+import sdmc_tools.access_ldms as access_ldms
+import datetime
+import os
+import pdb
+import datetime
+
+INPUT_DATA_PATH = '/trials/vaccine/p300/s001/qdata/LabData/ELISA_binding_pass-through/uploaded_by_lab/20260618-03/HVTN300 Binding Master File_v3.xlsx'
+
+
+# LDMS CHECK ---------------------------------------------------------------------------------------------------- #
+# read in data
+
+
+ds = pd.read_excel(INPUT_DATA_PATH, skiprows=3)
+
+
+
+COLUMN_NAMING = {
+    'Subject ID':'ptid',
+    'Visit':'visitno',
+    'Original ID':'guspec',
+    'Draw date':'drawdt'
+}
+
+ds = ds.rename(columns=COLUMN_NAMING)
+
+#drop the non-record row (it's a comment)
+ds.dropna(inplace=True, subset=['guspec'])
+
+# pull ldms
+ldms = access_ldms.pull_one_protocol('hvtn', 300)
+
+# subset ldms
+ldms = ldms.loc[ldms.guspec.isin(ds.guspec.dropna().unique())]
+
+# check ptid and visit
+m = ds.merge(ldms, on='guspec', how='left', suffixes=("_lab",""), indicator=True)
+m.txtpid = m.txtpid.astype(float)
+assert len(m.loc[(m.ptid!=m.txtpid) & (m.ptid.notna()),['ptid','txtpid']]) == 0
+
+m.vidval = m.vidval.astype(float)
+assert len(m.loc[(m.visitno!=m.vidval) & (m.visitno.notna()),['visitno','vidval']]) == 0
+
+#m['drawdt_ldms'] = m.apply(lambda row: datetime.date(int(row['drawdy']), int(row['drawdm']), int(row['drawdd'])) if not pd.isnull(row['drawdy']) else '', axis=1)
+m['drawdt_d'] = m.apply(lambda row: row['drawdt'].day, axis=1)
+m['drawdt_m'] = m.apply(lambda row: row['drawdt'].month, axis=1)
+m['drawdt_y'] = m.apply(lambda row: row['drawdt'].year, axis=1)
+
+
+assert len(m.loc[(m.drawdt_d!=m.drawdd) & (m.drawdt_d.notna()),['drawdt_d','drawdd']]) == 0
+assert len(m.loc[(m.drawdt_m!=m.drawdm) & (m.drawdt_m.notna()),['drawdt_m','drawdm']]) == 0
+assert len(m.loc[(m.drawdt_y!=m.drawdy) & (m.drawdt_y.notna()),['drawdt_y','drawdd']]) == 0
